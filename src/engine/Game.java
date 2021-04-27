ae3450d5d20010212445e0ace6644735ba2b7e44
KhaledNowara <K.Nowara02@gmail.com>
2021-04-27 16:45:22 +0200

    engine classs

--- src/engine/Game.java
+++ src/engine/Game.java
@@ -1,18 +1,43 @@
 package engine;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.io.FileReader;
+import java.io.BufferedReader;
 
 public class Game {
 
 	
 	private Player player;
 	private ArrayList<City> availabeCities;
-	private
-	private
-	private
+	private ArrayList<Distance> distances;
+	private int maxTurnCount;
+	private int currentTurnCount;
 
-	public Game() {
-		// TODO Auto-generated constructor stub
+	public Game(String playerName,String playerCity) throws IOException {
+	  this.player = new Player(playerName);
+
 	}
+
+	public static void readFile(String path) throws IOException{
+		String currentLine = "";
+		FileReader fileReader= new FileReader(path);
+		BufferedReader br = new BufferedReader(fileReader);
+		while ((currentLine = br.readLine()) != null) {
+		
+		// Parsing the currentLine String
+		}
+		br.close();
+		}
+
+	public void loadArmy(String cityName,String path) throws IOException{
+
+	}
+
+	private void loadCitiesAndDistances() throws IOException{
+
+		
+	}
+
 
 }
--- src/engine/Player.java
+++ src/engine/Player.java
@@ -13,8 +13,62 @@ public class Player {
 
 
 
+	public String getName() {
+		return name;
+	}
+
+
+
+	
+
+
+	public ArrayList<City> getControlledCities() {
+		return controlledCities;
+	}
+
+
+
+
+
+
+
+	public ArrayList<Army> getContArmies() {
+		return contArmies;
+	}
+
+
+
+
+
+
+	public double getTreasury() {
+		return treasury;
+	}
+
+
+
+	public void setTreasury(double treasury) {
+		this.treasury = treasury;
+	}
+
+
+
+	public double getFood() {
+		return food;
+	}
+
+
+
+	public void setFood(double food) {
+		this.food = food;
+	}
+
+
+
 	public Player(String name) {
 		this.name = name;
+		this.food = 0;
+		this.treasury = 0;
 	}
 
 }
